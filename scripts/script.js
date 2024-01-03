bd13fda5cb297925ae27ae55ee52bdaf06f0b256
EJ-Lan <ervinjon_lanada@outlook.com>
2024-01-03 01:58:50 -0700

    Make base constructor and function

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(book) {
+    if (book instanceof Book) {
+        myLibrary.push(book);
+    } else {
+        throw new Error("Argument must be a Book object");
+    }
+}
